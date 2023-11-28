75a794bf6d0b24c0934a589e4d3517ae963ae2ee
Adam Shimali <adam.shimali@gmail.com>
2023-11-28 12:54:08 +0000

    add design code reference page

--- application/blueprints/base/views.py
+++ application/blueprints/base/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-from application.models import DesignCode, Organisation
+from application.models import DesignCode, DesignCodeArea, Organisation
 
 base = Blueprint("base", __name__)
 
@@ -8,7 +8,6 @@ base = Blueprint("base", __name__)
 @base.route("/")
 def index():
     design_codes = DesignCode.query.all()
-
     return render_template("index.html", design_codes=design_codes)
 
 
@@ -19,3 +18,14 @@ def design_code(entity):
     return render_template(
         "design-code.html", design_code=design_code, organisation=organisation
     )
+
+
+@base.route("/design-code-area")
+def design_code_area():
+    design_code_reference = request.args.get("design_code_reference")
+    design_code_areas = [
+        dca
+        for dca in DesignCodeArea.query.all()
+        if dca.json is not None and dca.json["design-code"] == design_code_reference
+    ]
+    return render_template("design-code-area.html", design_code_areas=design_code_areas)

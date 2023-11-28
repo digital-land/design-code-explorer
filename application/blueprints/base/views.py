2a3e05e87b7b79e80f04e930c6e0a9fb5074f07b
Adam Shimali <adam.shimali@gmail.com>
2023-11-28 13:44:28 +0000

    re org index pages for codes and areas

--- application/blueprints/base/views.py
+++ application/blueprints/base/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template
 
 from application.models import DesignCode, DesignCodeArea, Organisation
 
@@ -7,25 +7,56 @@ base = Blueprint("base", __name__)
 
 @base.route("/")
 def index():
-    design_codes = DesignCode.query.all()
-    return render_template("index.html", design_codes=design_codes)
+    return render_template("index.html")
+
+
+@base.route("/design-codes")
+def design_codes():
+    dcs = DesignCode.query.all()
+    organisations = Organisation.query.all()
+    return render_template(
+        "design-codes.html", design_codes=dcs, organisations=organisations
+    )
+
+
+@base.route("/design-code-areas")
+def design_code_areas():
+    dcas = DesignCodeArea.query.all()
+    organisations = Organisation.query.all()
+    return render_template(
+        "design-code-areas.html",
+        design_code_areas=dcas,
+        organisations=organisations,
+    )
 
 
 @base.route("/design-code/<int:entity>")
 def design_code(entity):
-    design_code = DesignCode.query.get(entity)
-    organisation = Organisation.query.get(design_code.organisation_entity)
-    return render_template(
-        "design-code.html", design_code=design_code, organisation=organisation
-    )
-
-
-@base.route("/design-code-area")
-def design_code_area():
-    design_code_reference = request.args.get("design_code_reference")
+    dc = DesignCode.query.get(entity)
+    organisation = Organisation.query.get(dc.organisation_entity)
     design_code_areas = [
         dca
         for dca in DesignCodeArea.query.all()
-        if dca.json is not None and dca.json["design-code"] == design_code_reference
+        if dca.json is not None and dca.json["design-code"] == dc.reference
     ]
-    return render_template("design-code-area.html", design_code_areas=design_code_areas)
+    return render_template(
+        "design-code.html",
+        design_code=dc,
+        organisation=organisation,
+        design_code_areas=design_code_areas,
+    )
+
+
+@base.route("/design-code-area/<int:entity>")
+def design_code_area(entity):
+    dca = DesignCodeArea.query.get(entity)
+    design_code = DesignCode.query.filter(
+        DesignCode.reference == dca.json["design-code"]
+    ).first()
+    organisation = Organisation.query.get(dca.organisation_entity)
+    return render_template(
+        "design-code-area.html",
+        design_code_area=dca,
+        organisation=organisation,
+        design_code=design_code,
+    )
